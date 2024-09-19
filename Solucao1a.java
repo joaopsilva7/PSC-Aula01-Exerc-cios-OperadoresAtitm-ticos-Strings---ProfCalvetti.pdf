d58b63e252dc32877d22e4c95c97d937ed1a6966
Joaopsilva7_ <joaomarcos9316@gmail.com>
2024-09-19 12:44:20 -0300

    Add files via upload

---
+++ Solucao1a.java
@@ -0,0 +1,7 @@
+public class Solucao1a {
+    public static void main(String[] args) {
+        int idade = 18; 
+        int diasVividos = idade * 365;
+        System.out.println("Você viveu aproximadamente " + diasVividos + " dias.");
+    }
+}
